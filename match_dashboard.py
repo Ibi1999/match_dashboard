8c6af1e01e80bc408cf62077564bf2d53ef69cf4
Ibi1999 <ibrahimhuseyin23@gmail.com>
2025-06-11 19:07:57 +0100

    Update match_dashboard.py

--- match_dashboard.py
+++ match_dashboard.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 st.set_page_config(layout="wide")
 
 # Load and display logo in the top-left
-from PIL import Image
-logo = Image.open("logo.png")
+# from PIL import Image
+# logo = Image.open("logo.png")
 
-col1, col2 = st.columns([1, 6])
-with col1:
-    st.image(logo, width=80)  # Adjust width as needed
+# col1, col2 = st.columns([1, 6])
+# with col1:
+#     st.image(logo, width=80)  # Adjust width as needed
 st.markdown("<small>Created by Ibrahim Oksuzoglu</small>", unsafe_allow_html=True)
 
 @st.cache_data
